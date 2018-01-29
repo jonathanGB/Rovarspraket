9b9061e079e9ad24c236bee10366e7667b21ab03
Kyle Kung <kkung065@uottawa.ca>
2018-01-29 15:14:41 -0500

    allow the user to input a string to be rovarspraketed

--- rovarspraket.java
+++ rovarspraket.java
@@ -1,4 +1,10 @@
-class Rovarsparket {
+import java.util.Scanner;
+
+class rovarspraket {
 	public static void main(String[] args){
+		System.out.println("Please Enter a Sentence to be Rövarspråketed");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		System.out.println("Check the input: " + input);
 	}	
 }
